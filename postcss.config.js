6ed2933672f53f8403f1d7e0ba9f10dd84212ab1
tamakiii <tamakiii@users.noreply.github.com>
2022-11-19 10:50:39 +0900

    add stylelint

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-preset-env'),
+    require('stylelint'),
     require('autoprefixer'),
     require('cssnano'),
   ]
